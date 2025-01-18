c1893347400ecdfb566343885060b391c3066f03
Timothy Hays <timothy.hays@bc.edu>
2025-01-18 16:10:46 -0500

    Initial Commit

---
+++ Football/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Football
+//
+//  Created by Tim Hays on 1/18/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Tim Hays on 1/18/25.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
